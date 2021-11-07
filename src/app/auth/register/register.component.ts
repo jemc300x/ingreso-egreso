2684cd17d54596d9398deade024924e34de6f833
Enrique Moreno <jemc300x@gmail.com>
2021-11-07 14:05:28 -0500

    add: Reactive Forms

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      nombre:   ['', Validators.required],
+      correo:   ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onRegister() {
+    console.log(this.registerForm.value);
   }
 
 }
